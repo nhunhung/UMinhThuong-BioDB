f733dff35f8f8b95cf291d7864010317f7bc370f
tuantran24 <tuantrann24@gmail.com>
2024-12-06 17:06:06 +0700

    Khôi phục dữ liệu

---
+++ OneDrive/Máy tính/quoctuan/build/precache-manifest.e707e3b1d8cd135a5ea3ec0b1826eb7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb621be9dc4c9521c46620360f7325d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b14d63993ad9156cba",
+    "url": "/static/css/main.a324cc2d.chunk.css"
+  },
+  {
+    "revision": "f1b971b64c940199c136",
+    "url": "/static/js/2.31a07024.chunk.js"
+  },
+  {
+    "revision": "ba71f4f2c925f1d7c5ee",
+    "url": "/static/js/3.9093b10f.chunk.js"
+  },
+  {
+    "revision": "40b14d63993ad9156cba",
+    "url": "/static/js/main.0f79c185.chunk.js"
+  },
+  {
+    "revision": "bfc0ad2873a81f923c5b",
+    "url": "/static/js/runtime~main.d20650d8.js"
+  }
+]);

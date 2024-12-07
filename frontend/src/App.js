897a5804aa61ee518bafd3b3abf516a230fd09dc
dattanlee@gmail.com <dattanlee@gmail.com>
2024-12-07 14:44:20 +0700

    Thêm chức năng liên hệ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
+// App.js
 import React from 'react';
-import LoginForm from './components/LoginForm';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginForm from './components/LoginForm'; 
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/contact" element={<ContactForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

1c003980f9ed313f5c2d3d494f95c7461563898d
NhuNhung <ngnhung4685@gmail.com>
2024-12-11 17:46:04 +0700

    fixfooter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 // src/App.js
 import './App.css';
-import { Container } from 'postcss';
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import AdminLoginForm from './components/AdminLoginForm';
 import UploadExcel from './components/UploadExcel';
 import Admin from './pages/Admin';
@@ -11,29 +10,30 @@ import Footer from './layout/footer';
 import { LanguageProvider } from './components/LanguageContext';
 import Home from './components/Home';
 
+function FooterWrapper() {
+  const location = useLocation();
+
+  // Chỉ hiển thị footer ở đường dẫn "/"
+  return location.pathname === '/' ? <Footer /> : null;
+}
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <LanguageProvider>
       <Router>
-      <Header />
-
+        <Header />
 
         <Routes>
-
-          {/* <Route path="/" element={<Navigate to="/admin" />} /> */}
-          {/* Trang đăng nhập */}
-          {/* <Route path="/admin" element={isAuthenticated ? <Admin /> : <AdminLoginForm />} /> */}
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/login" element={<AdminLoginForm />} />
           <Route path="/upload" element={<UploadExcel />} />
         </Routes>
-        
-      </Router>
-      <Footer />
 
+        <FooterWrapper />
+      </Router>
     </LanguageProvider>
   );
 }
